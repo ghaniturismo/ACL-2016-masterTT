590753f85eb5b4ad76b389296bf61a1f51f219ed
Hturi <ghaniturismo@gmail.com>
2016-11-23 18:08:23 +0100

    ADD AlienTest.java

---
+++ core/test/fr/ul/acl/model/AlienTest.java
@@ -0,0 +1,22 @@
+package fr.ul.acl.model;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AlienTest {
+	private float speed;
+	private float delta = (float) 0.017519908;
+
+	@Test
+	public void AlienGoDown() {
+		World world = new World();
+		Alien alien = world.getAlien();
+		speed = alien.getSpeed();
+		float tmp = speed * delta;
+		float tmp1 = alien.getPosition().y - tmp;
+		alien.updateAlien(delta);
+		float y = alien.getPosition().y;
+		assertTrue( tmp1 == y );
+	}
+
+}
